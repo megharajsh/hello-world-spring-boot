cf7f7f66012c1e00dd35b88a1b6b2538aa1bf1d2
Megharaj S H <msh@vmware.com>
2020-12-14 14:53:26 +0530

    Reading from application properties

--- src/main/java/io/tanzu/workshop/controller/GreetingController.java
+++ src/main/java/io/tanzu/workshop/controller/GreetingController.java
@@ -1,14 +1,18 @@
 package io.tanzu.workshop.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
+    @Value("$message")
+    private String message;
+
     // http://localhost:8080/
     @GetMapping(path = "/")
     public String greet() {
-        return "Hello World!";
+        return message;
     }
 }
